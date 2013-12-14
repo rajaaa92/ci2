eea81a276ddd65ad788db77e729e6594761e1467
Joanna Rajewska <joanna.rajewska@netguru.pl>
2013-12-14 15:41:46 +0000

    clean up

--- stego.c
+++ stego.c
@@ -58,26 +58,5 @@ int main (int argc, char **argv) {
   } else if (argv[1][0] == 'u') {
     printf("u\n");
   }
-
-  // unsigned char position;
-  // int i;
-  // dct* dict = new_dict();
-  // wrd* P, *P_C, *C, *empty;
-  // C = new_word(); empty = new_word();
-  // while(fread(&position, sizeof(unsigned char), 1, input)) {
-  //   alloc_word(C, 1); P = NULL; P_C = new_word();
-  //   int c = fread(C->body, sizeof(char), 1, input);
-  //   if (position) { P = &dict->words_array[position]; }
-  //   else P = empty;
-  //   sum_words(P_C, P, C);
-  //   if (c) fwrite(P_C->body, sizeof(char), P_C->max_size, output);
-  //   else fwrite(P->body, sizeof(char), P->max_size, output);
-  //   add_word_to_dict(dict, P_C);
-  //   free(C->body); free_word(C);
-  //   free(P_C->body); free_word(P_C);
-  // }
-  // for(i = 0; i <= dict->size; ++i) free(dict->words_array[i].body);
-  // free(dict->words_array);
-  // free(dict);
   return 0;
 }
