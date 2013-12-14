d7897884ab1fe11649047b9e0d55cc4457e77309
Joanna Rajewska <joanna.rajewska@netguru.pl>
2013-12-14 15:41:36 +0000

    copy the image taking a letter from the message every pixel

--- stego.c
+++ stego.c
@@ -6,8 +6,8 @@
 
 int main (int argc, char **argv) {
   FILE *msg, *image_changed, *image;
-  unsigned char character;
-  int i = 0, biale = 0, razem = 0;
+  unsigned char pixel_part, pixel_changed_part, character, rgorb = 0; // r = 0, g = 1, b = 2;
+  int i = 0, loaded = 0;
 
   // hiding option chosen
   if (argv[1][0] == 'h') {
@@ -17,15 +17,37 @@ int main (int argc, char **argv) {
     image_changed = fopen(argv[4], "w");
 
     // copying the header
-    while(fread(&character, sizeof(unsigned char), 1, image) && (i < 73)) {
-      fwrite(&character, sizeof(unsigned char), 1, image_changed);
+    while(fread(&pixel_part, sizeof(unsigned char), 1, image) && (i < 73)) {
+      fwrite(&pixel_part, sizeof(unsigned char), 1, image_changed);
       ++i;
-    } fwrite(&character, sizeof(unsigned char), 1, image_changed);
+    } fwrite(&pixel_part, sizeof(unsigned char), 1, image_changed);
 
-    // copying the rest of the image
-    while(fread(&character, sizeof(unsigned char), 1, image)) {
-      fwrite(&character, sizeof(unsigned char), 1, image_changed);
-      ++i;
+    // copying the rest of the image with changes
+    while(fread(&pixel_part, sizeof(unsigned char), 1, image)) {
+      pixel_changed_part = pixel_part;
+      // take one letter from the msg if we are handling a new pixel
+      if (rgorb == 0) { loaded = fread(&character, sizeof(unsigned char), 1, msg); }
+      if (loaded == 0) { break; } // if there is no msg left - dont change the pixels
+      // which part of the pixel will we change now?
+      switch (rgorb) {
+        case 0: // red, the first part - change 3 last bits
+          // bierz trzy pierwsze bity charactera i wklej je do trzech ostatnich bitow pixel_changed_parta
+        break;
+        case 1: // green, the second part - change 3 last bits
+          // bierz 4-6 bity charactera i wklej je do trzech ostatnich bitow pixel_changed_parta
+        break;
+        case 2: // blue, the third part - change 2 last bits
+          // bierz 2 ostatnie bity charactera i wklej je do dwoch ostatnich bitow pixel_changed_parta
+        break;
+      }
+      printf(".");
+      fwrite(&pixel_changed_part, sizeof(unsigned char), 1, image_changed); // write the pixel's part with changes
+      rgorb += 1; rgorb %= 3; // what will be the next pixel's part - r, g or b?
+    } fwrite(&pixel_changed_part, sizeof(unsigned char), 1, image_changed);
+
+    // copying the header
+    while(fread(&pixel_part, sizeof(unsigned char), 1, image)) {
+      fwrite(&pixel_part, sizeof(unsigned char), 1, image_changed);
     }
 
     fclose(msg);
